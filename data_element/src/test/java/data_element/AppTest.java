95fdcdcc9e68a12e4d00342252687509780df966
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-10-24 10:56:47 -0600

    ah

---
+++ data_element/src/test/java/data_element/AppTest.java
@@ -0,0 +1,18 @@
+package data_element;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testApp() {
+        assertEquals(1, 1);
+    }
+}
