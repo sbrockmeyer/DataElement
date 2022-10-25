3d732772cbf6b29229e01611453597b581a50bc3
sobrien-eng <sobrien@student.neumont.edu>
2022-10-25 09:58:13 -0600

    eee

--- data_element/src/main/java/data_element/App.java
+++ data_element/src/main/java/data_element/App.java
@@ -1,9 +1,14 @@
 package data_element;
-
+import java.io.*;  
+import java.util.Scanner; 
 /**
  * Hello world!
  */
 public final class App {
+    public class Person (){
+        String name;
+        String phone;
+    }
     private App() {
     }
 
@@ -11,7 +16,31 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         System.out.println("Hello World!");
+        Scanner sc = new Scanner (new File("data_element/people-2.to.regex.csv"));
+        sc.useDelimiter(",");
+        while(sc.hasNext()){
+            System.out.println(sc.next());
+        }
+        sc.close();
     }
+    
+  
+    public static boolean isPhoneNumber(String maybePhone) {
+        // String hardCode = "123-456-7890";
+        if (maybePhone == null) {
+            return false;
+        }
+        String regexString = "[0-9]{3}-[0-9]{3}-[0-9]{4}";
+        return maybePhone.matches(regexString);
+    }
+    public static boolean isName(String maybeName){
+        if (maybeName == null){
+            return false;
+        }
+        String regex = "([A-Z][a-zA-Z-.']* *)+";
+        return maybeName.matches(regex);
+    }
+
 }
