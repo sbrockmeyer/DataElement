2e065e278cda9c641be6bf9051e5f9cf9178d8a9
sbrockmeyer <sbrockmeyer@student.neumont.edu>
2022-10-25 13:50:12 -0600

    ah

--- data_element/src/main/java/data_element/App.java
+++ data_element/src/main/java/data_element/App.java
@@ -1,14 +1,49 @@
 package data_element;
 import java.io.*;  
-import java.util.Scanner; 
+import java.util.Scanner;
+
+import org.apache.commons.collections4.functors.CatchAndRethrowClosure;
+
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvBindAndJoinByPosition;
+import com.opencsv.bean.CsvBindByPosition;
+import com.opencsv.exceptions.CsvException; 
+
 /**
  * Hello world!
  */
 public final class App {
-    public class Person (){
+    public class Person{
+
+        @CsvBindByPosition(position = 0)
         String name;
+
+        @CsvBindByPosition(position = 3)
         String phone;
+        
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+        
+        public String getPhone() {
+            return phone;
+        }
+
+        public void setPhone(String phone) {
+            this.phone = phone;
+        }
+
+        @Override
+        public String toString() {
+            // TODO Auto-generated method stub
+            return super.toString();
+        }
     }
+    
     private App() {
     }
 
@@ -16,17 +51,32 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program.
      */
+
     public static void main(String[] args) throws Exception{
         System.out.println("Hello World!");
-        Scanner sc = new Scanner (new File("data_element/people-2.to.regex.csv"));
-        sc.useDelimiter(",");
-        while(sc.hasNext()){
-            System.out.println(sc.next());
+
+        String line ="";
+        String splitBy = ",";
+
+        // Scanner sc = new Scanner (new File("data_element/people-2.to.regex.csv"));
+        // sc.useDelimiter(",");
+        // while(sc.hasNext()){
+        //     System.out.println(sc.next());
+        // }
+        // sc.close();
+
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("data_element/people-2.to.regex.csv"));
+            while((line = br.readLine()) != null){
+                String[] person = line.split(splitBy);
+                System.out.println();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
         }
-        sc.close();
+    
     }
     
-  
     public static boolean isPhoneNumber(String maybePhone) {
         // String hardCode = "123-456-7890";
         if (maybePhone == null) {
